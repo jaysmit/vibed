297042d579bb7f604f62532c373f4b6fffe972f0
Jake <jake_smidders@hotmail.com>
2026-08-27 14:37:17 +1000

    Simplify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,5 @@
 import type { NextConfig } from "next";
-import { dirname, resolve } from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const nextConfig: NextConfig = {
-  outputFileTracingRoot: resolve(__dirname),
-  experimental: {
-    turbo: {
-      root: resolve(__dirname),
-    },
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
